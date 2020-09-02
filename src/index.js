6f474a8d50e6e3ad184ddcbe8e064bccdf704771
blueblackzebra <sparshkasana99@gmail.com>
2020-09-02 16:59:57 +0530

    CORS support

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express=require('express')
+const cors=require('cors')
 require('./db/database')
 const userRouter=require('./routers/userRouter')
 const subRouter=require('./routers/subRouter')
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use(userRouter)
 app.use(subRouter)
 app.use(adminRouter)
+app.use(cors())
 
 app.listen(port, ()=> {
     console.log("Server is up on "+port)
