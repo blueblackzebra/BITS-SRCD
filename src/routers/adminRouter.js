f0e0c96ff802823570bbd7731109a79a82690fc7
blueblackzebra <sparshkasana99@gmail.com>
2020-09-27 16:31:23 +0530

    Npm modules updated and email functionality added

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -1,8 +1,12 @@
 const express = require("express")
-const router = new express.Router();
+const router = new express.Router()
+const sgMail=require('@sendgrid/mail')
 
 const Admin=require('../models/admin')
 const Sub=require('../models/submissions')
+const User=require('../models/user')
+
+sgMail.setApiKey(process.env.SG_KEY)
 
 
 router.post('/admin/new', async (req, res) => {
@@ -20,6 +24,20 @@ router.patch('/admin/mark/:id', async (req,res) => {
     try {
         const sub= await Sub.findByIdAndUpdate(req.params.id, {status: true}, {new: true})
 
+        const currUser= await User.findById(sub.owner)
+        const currEmail=currUser.email;
+
+        const msg= {
+            to: currEmail,
+            from: 'bitssrcd@gmail.com',
+            subject: 'Test1',
+            text: 'Submission approved'
+        }
+
+        console.log(currEmail)
+
+        sgMail.send(msg);
+
         res.status(200).send()
     } catch(e) {
         res.status(400).send()
@@ -29,6 +47,19 @@ router.patch('/admin/mark/:id', async (req,res) => {
 router.patch('/admin/comment/:id', async(req,res) => {
     try {
         const sub= await Sub.findByIdAndUpdate(req.params.id, {comment: req.body.comment}, {new: true})
+
+        const currUser= await User.findById(sub.owner)
+        const currEmail=currUser.email;
+
+        const msg= {
+            to: currEmail,
+            from: 'bitssrcd@gmail.com',
+            subject: 'Test2',
+            text: 'Comment added :- '+req.body.comment
+        }
+
+        sgMail.send(msg);
+
 
         res.status(200).send()
     } catch(e) {
